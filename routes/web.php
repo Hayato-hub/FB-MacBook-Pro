9715f814f8eb084e902bab29e137e5d0911ab150
EC2 Default User <ec2-user@ip-172-31-20-152.us-east-2.compute.internal>
2020-06-26 22:21:33 +0000

    mm

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,32 @@
 |
 */
 
-Route::get('app', function () {
+Route::get('job', function () {
     return view('fb.check'); //仮LP
-    // return view('fb.live'); //獲得LP
+    // return view('fb.job'); //獲得用LP
 });
 
-Route::get('dl', function () {
-    return view('fb.dl'); //自動遷移LP。ココに案件タグを設置
+//イククル-DL
+Route::get('app', function () {
+    return view('fb.app1'); //自動遷移LP。ココに案件タグを設置
+});
+
+//popo
+Route::get('install', function () {
+    return view('fb.install'); //自動遷移LP。ココに案件タグを設置
 });
 
 // LP確認用
 Route::get('cccheck', function () {
-    return view('fb.live'); //獲得LP
+    return view('fb.job'); //獲得LP
 });
+
+// //男性向け出会えない系 - 獲得用
+// Route::get('app', function () {
+//     return view('fb.check'); //仮LP
+//     // return view('fb.live'); //獲得LP
+// });
+// //男性向け出会えない系 - DL遷移用
+// Route::get('dl', function () {
+//     return view('fb.dl'); //自動遷移LP。ココに案件タグを設置
+// });
