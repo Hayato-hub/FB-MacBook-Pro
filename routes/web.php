127d5089261f427d74c3c0bee4f9366bc60cad88
EC2 Default User <ec2-user@ip-172-31-20-152.us-east-2.compute.internal>
2020-07-24 23:17:58 +0000

    Mm

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,16 @@
 // LP獲得用
 Route::get('match', function () {
     return view('fb.check'); //確認用LP
-    // return view('fb.noMatchRanking'); //獲得LP(3)
-    
     // return view('fb.noMatch'); //獲得LP(2)
+    
+    // return view('fb.noMatchRanking'); //獲得LP(3)
     // return view('fb.match'); //獲得LP(1)
 });
 
 // LP確認用
 Route::get('cccheck', function () {
-    return view('fb.noMatchRanking'); //獲得LP(3)
+    return view('fb.noMatch'); //獲得LP(2)
     
-    // return view('fb.noMatch'); //獲得LP(2)
+    // return view('fb.noMatchRanking'); //獲得LP(3)
     // return view('fb.match'); //獲得LP(1)
 });
