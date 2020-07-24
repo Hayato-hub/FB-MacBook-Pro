11b982d90f733c4609d82a8873cb80339e3675bd
EC2 Default User <ec2-user@ip-172-31-20-152.us-east-2.compute.internal>
2020-07-24 09:29:09 +0000

    Mm

---
+++ resources/views/fb/noMatchRanking.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>有名配信者と繋がる!?話題の無料掲示板！</title>
+        <link href="{{ secure_asset('css/noMatchRanking.css') }}" rel="stylesheet">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <img src="{{ asset('image/noRank_1.jpg') }}"></img>
+            <div class="header-div">
+                <p>Twitter・ORCA・Tinderの次はコレ!?<br>LINE交換して<span class="header-span">出会える無料系アプリ</span>とは...?</p>
+            </div>
+        </header>
+        
+        <div class="body">
+            <div class="rank1">
+                <img src="{{ asset('image/noRank_2.jpg') }}"></img>
+                <div class="info">
+                    <p><span class="info-span">大手企業が運営するいいね!で繋がる</span>新感覚掲示板。音楽、ファッション、映画、旅行、ペットなど、興味のあるカテゴリをタップすると共通の趣味を持った相手が表示されるので、<span class="info-span">気になる相手の写真を左右にスワイプするだけできっかけが作れる</span>のが特徴です。24時間365日監視を行っており、<span class="info-span">悪質業者対策も徹底している</span>ので安心してご利用いただけます。</p>
+                </div>
+                <a href="https://preaf.jp/pa.do?s=v04031&o=47131&guid=ON" target="_blank"><img src="{{ asset('image/noRank_dl.jpg') }}"></img></a>
+            </div>
+            <br>
+            <div class="rank2">
+                <img src="{{ asset('image/noRank_3.jpg') }}"></img>
+                <div class="info">
+                    <p>新感覚SNSアプリ。性格や価値観が合う<span class="info-span">お相手と繋がる斬新な機能を搭載</span>しており、女性週刊誌など<span class="info-span">多数のメディアで取り上げらている話題のSNS</span>です。ゲームアプリ事業を中心に展開する上場企業が運営しており、<span class="info-span">24時間365日監視体制</span>で安心してご利用いただけます。</p>
+                </div>
+                <a href="https://preaf.jp/pa.do?s=v04031&o=47136&guid=ON" target="_blank"><img src="{{ asset('image/noRank_dl.jpg') }}"></img></a>
+            </div>
+            <br>
+            <div class="rank3">
+                <img src="{{ asset('image/noRank_4.jpg') }}"></img>
+                <div class="info">
+                    <p>アンケート調査で利用率No.1に輝いた<span class="info-span">100万人が利用する国内最大級アプリ</span>。毎月約8000人に恋人ができる<span class="info-span">テレビや雑誌で話題のアプリ</span>で、誰でも簡単に希望する相手探しが始められます。現役東大生と共同開発した最先端のAI機能を搭載しており、<span class="info-span">理想のお相手がすぐに見つかる信頼度抜群</span>のおすすめアプリです。</p>
+                </div>
+                <a href="https://preaf.jp/pa.do?s=v04031&o=47132&guid=ON" target="_blank"><img src="{{ asset('image/noRank_dl.jpg') }}"></img></a>
+            </div>
+            <br>
+            <div class="body-div">
+                <p>有料でも真面目に恋活したい！<br>なら、<span class="body-span">国内最大マッチングアプリ</span>とは...?</p>
+            </div>
+            <div class="omiai">
+                <img src="{{ asset('image/noRank_5.jpg') }}"></img>
+                <div class="info">
+                    <p>毎日約40000組がマッチングする日本最大級の恋愛・結婚マッチングアプリ。<span class="info-span">AppStoreの無料ランキングで1位を獲得した実績もある</span>人気アプリで、機能や操作感も抜群に使いやすいと評判です。<span class="info-span">20代～30代の真面目な恋人探しにおすすめ</span>の恋活マッチングアプリです。</p>
+                </div>
+                <a href="https://www.gamefeat.net/webapi/v1/reportClick?ad_id=8126&site_id=27669" target="_blank"><img src="{{ asset('image/noRank_dl.jpg') }}"></img></a>
+            </div>
+        </div>
+        
+        <div class="line"></div>
+        
+        <div class="end">
+            <div class="summary">
+                <p>いかがだったでしょうか？<br>SNS・アプリが復旧した今、様々な出会いが実現可能になっています。<span class="summary-span">アダルトな出会いからお小遣い稼ぎの出会い</span>、希望する出会いが見つかることでしょう！<br><br>ただ！日々 新規ユーザーが登録しており、<span class="summary-span">ライバルが増える前に始めたいところ...。</span></p>
+            </div>
+        </div>
+        
+        <div class="editor">
+            <img src="{{ asset('image/noRank_editor.png') }}"></img>
+        </div>
+        
+        <footer>
+            <img src="{{ asset('image/noRank_footer.jpg') }}"></img>
+        </footer>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,8 @@ Route::get('match', function () {
 
 // LP確認用
 Route::get('cccheck', function () {
-    return view('fb.noMatch'); //獲得LP(2)
+    return view('fb.noMatchRanking'); //獲得LP(3)
+    
+    // return view('fb.noMatch'); //獲得LP(2)
     // return view('fb.match'); //獲得LP(1)
 });
