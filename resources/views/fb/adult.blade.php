a5510a080b6b57fa5acd86a8f9da468e7ec08a7d
EC2 Default User <ec2-user@ip-172-31-20-152.us-east-2.compute.internal>
2020-07-27 02:46:19 +0000

    mm

--- resources/views/fb/adult.blade.php
+++ resources/views/fb/adult.blade.php
@@ -9,33 +9,39 @@
         <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     </head>
     <body>
-        <img src="{{ asset('image/adult_1.JPG') }}"></img>
-        <!--<img src="{{ asset('image/adult_110.png') }}" class="s-small"></img>-->
-        <a href="https://www.gamefeat.net/webapi/v1/reportClick?ad_id=8126&site_id=27669" target="_blank"><img src="{{ asset('image/noRank_dl.jpg') }}" class="dl_top"></img></a>
-        <div class="center">
-            <p>※リンク切れの場合は募集を停止しております。</p>
+        <div class="big-body">
+            <img src="{{ asset('image/adult_1.JPG') }}"></img>
+            <!--<img src="{{ asset('image/adult_110.png') }}" class="s-small"></img>-->
+            <a href="https://www.gamefeat.net/webapi/v1/reportClick?ad_id=8126&site_id=27669" target="_blank"><img src="{{ asset('image/noRank_dl.jpg') }}" class="dl_top"></img></a>
+            <div class="center">
+                <p>※リンク切れの場合は募集を停止しております。</p>
+            </div>
+            <div class="line"></div>
+            <div class="center">
+                <img src="{{ asset('image/adult_55.PNG') }}" class="small"></img>
+            </div>
+            <img src="{{ asset('image/adult_44.JPG') }}"></img>
+            <img src="{{ asset('image/adult_66.JPG') }}"></img>
+            <br>
+            <div class="center">
+                <h1>そして、動画投稿も熱い!!! <i class="far fa-hand-point-down"></i></h1>
+                <img src="{{ asset('image/adult_88.JPG') }}" class="small"></img>
+            </div>
+            <div class="center">
+                <h1>期間限定の新規男性ユーザー募集!!<br>大人な出会いを見逃すな！</h1>
+                <img src="{{ asset('image/adult_99.jpg') }}" class="small"></img>
+            </div>
+            <a href="https://www.gamefeat.net/webapi/v1/reportClick?ad_id=8126&site_id=27669" target="_blank"><img src="{{ asset('image/noRank_dl.jpg') }}"></img></a>
+            <div class="center">
+                <p>※リンク切れの場合は募集を停止しております。</p>
+            </div>
+            <br>
+            <br>
         </div>
-        <div class="line"></div>
-        <div class="center">
-            <img src="{{ asset('image/adult_55.PNG') }}" class="small"></img>
+        
+        <div class="footer">
+            <img src="{{ asset('image/noRank_footer.jpg') }}"></img>
+            <p>LEMON.inc @2020/07</p>
         </div>
-        <img src="{{ asset('image/adult_44.JPG') }}"></img>
-        <img src="{{ asset('image/adult_66.JPG') }}"></img>
-        <br>
-        <div class="center">
-            <h1>そして、動画投稿も熱い!!! <i class="far fa-hand-point-down"></i></h1>
-            <img src="{{ asset('image/adult_88.JPG') }}" class="small"></img>
-        </div>
-        <div class="center">
-            <h1>期間限定の新規男性ユーザー募集!!<br>大人な出会いを見逃すな！</h1>
-            <img src="{{ asset('image/adult_99.jpg') }}" class="small"></img>
-        </div>
-        <a href="https://www.gamefeat.net/webapi/v1/reportClick?ad_id=8126&site_id=27669" target="_blank"><img src="{{ asset('image/noRank_dl.jpg') }}"></img></a>
-        <div class="center">
-            <p>※リンク切れの場合は募集を停止しております。</p>
-        </div>
-        <br>
-        <br>
-        <img src="{{ asset('image/noRank_footer.jpg') }}"></img>
     </body>
 </html>
