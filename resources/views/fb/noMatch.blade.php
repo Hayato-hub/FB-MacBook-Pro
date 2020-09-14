b3ab7003001c4da9eac06b5e8e85b1c2d3af98c4
EC2 Default User <ec2-user@ip-172-31-20-152.us-east-2.compute.internal>
2020-09-14 23:10:41 +0000

    Mm

--- resources/views/fb/noMatch.blade.php
+++ resources/views/fb/noMatch.blade.php
@@ -4,27 +4,27 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>激ヤバ話題まとめ@5ch</title>
+        <title>裏垢女子まとめ@5ch</title>
         <link href="{{ secure_asset('css/noMatch.css') }}" rel="stylesheet">
         <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     </head>
     <body>
         <header>
-            <p>激ヤバ話題まとめ@5ch</p>
+            <p>裏垢女子まとめ@5ch</p>
         </header>
         
         <div class="big-body">
             <div class="body-top">
-                <h1><span>承認欲求の塊！割とガチでえぐいw<br>裏垢女子がこぞって脱いでオナ電・生オナ配信してるwww</span></h1>
-                <a href="https://www.gamefeat.net/webapi/v1/reportClick?ad_id=8126&site_id=27669" target="_blank"><img src="{{ asset('image/uraLive_1.JPG') }}"></a>
-                <a href="https://www.gamefeat.net/webapi/v1/reportClick?ad_id=8126&site_id=27669" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 公式AppStoreはコチラ <i class="fas fa-external-link-alt"></i></a>
-                <p>生配信のスクショ! IDで検索すれば過去配信閲覧できるでー</p>
+                <h1><span>AVに飽きた人必見!!!<br>ガチ素人の『裏配信』が今熱い！</span></h1>
+                <a href="https://a-trade.jp/redirect/eazy?media=G16531" target="_blank"><img src="{{ asset('image/uraLive_1.JPG') }}"></a>
+                <h1><span>個人配信なら『見せ合い』も可能..!?</span></h1>
+                <a href="https://a-trade.jp/redirect/eazy?media=G16531" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 裏配信はコチラ <i class="fas fa-external-link-alt"></i></a>
             </div>
             
             <div class="big-body-main">
                 <div class="body-main">
-                    <h1>TwitterやTikTokフォローを求めて裏垢女子がわんさか！！もう規制前のアダルト掲示板・・・</h1>
-                    <p>2020年07月11日20:03</p>
+                    <h1>インスタ・ニコ生・TikTokで垢BANされた裏垢女子多数！<br>刺激強すぎる裏配信の実態とは..!?</h1>
+                    <p>2020年09月14日20:03</p>
                 </div>
                 
                 <div class="body-main-2">
@@ -32,53 +32,36 @@
                 </div>
                 
                 <div class="body-main-3">
-                    <h3>生配信がエグい！マンコまる見えだし清楚系からギャル、カップル配信も・・・</h3>
-                    <p>正直、ほんまにエグい。Pornhubとか他のアダルトサイトが全然かわいいくらいリアルな配信。むっちゃ興奮する。<br><br>だって、普通にTikTokとかあげてる子がエロ配信してるねんで？それも丸出しで。<br><br>最近はカップルちゃんねるもエロ配信始めたとか話題になって、、、まじで規制前の今だから盛り上がってる感じ！</p>
-                    <br>
-                    <h3>オナ電で相互オナ・見せ合いもイケる！電話セックスってまじで興奮するからやってみて！</h3>
-                    <p>気になる女の子見つけたらオナ電誘ってみて！そうゆう目的のユーザーしかおらんから、返信くれればだいたいいける！<br><br>カメラ越しの映像とかまじで最高！普通に黙ってスクショしちゃったわwww</p>
+                    <h3>指入れ・おもちゃ・見せ合い・鑑賞 etc...<br>ガチ裏垢女子が見つかるライブ配信</h3>
+                    <p>有名SNSを垢BANされただけあって、刺激強い配信が多数。Pornhubやdmm(Fanza)が飽きた人、リアルな素人のエロ動画を見たい人は必見。<br><br>オナニーだけでも種類は豊富。指入れやクリトリス、おもちゃプレイは勿論、個人配信で繋がれば<span class="red">あなたがオナニー指示を出してもOK</span>。</p>
                     <img src="{{ asset('image/uraLive_3.JPG') }}">
                     <br>
                     <br>
-                    <h3>男同士で情報交換できるのもでかい！おすすめ紹介は勿論、ヤレる女情報とかもありがたいwww</h3>
-                    <p>基本ここを使ってる男性はエロ界隈に詳しい人が多いから、ヤレる女とかセフレ交換とかの情報も熱い！<br><br>大都市は勿論、俺が住んでる奈良とか地方都市の情報もあるのでありがたい。。。女の子のTwitter ID載せてくれるしまじで使えるwww<br><br>Tinderで頑張って女引っ掛けてたのがバカらしいわwww</p>
+                    <h3>性欲強め女子多数なので<br>SNS交換して実際にオフパコするケースも..!?</h3>
+                    <p>中にはTwitterIDなどを公開してるライバーさんもおり、<span class="red">定期的にオフ会を開催</span>しているケースもあります。<br><br>オフ会でなにをするかは言わずもがな。</p>
                     <br>
-                    <h3>最近使い過ぎてデータフォルダがやばい件wwwww</h3>
+                    <h3>ライブ配信を保存してると...<br>データフォルダに要注意！</h3>
+                    <p>アーカイブ機能は無いので、貴重なライブ配信は保存して見逃すな。</p>
                     <img src="{{ asset('image/uraLive_4.JPG') }}">
-                    <p>配信動画保存してたらまじでこうなるから気をつけてwww<br><br>ここはJC・JKゾーンだけど他にもギャル、美魔女、カップル系も！Twitterに流したら自分のフォロワー増える?かもなwww</p>
                     <br>
-                    <h3>使い方は簡単！TwitterとかYouTube使えてたら分かるはず！</h3>
-                    <p>TwitterみたいにIDで検索して女の子探したり、YouTubeみたいにキーワード検索してライブ配信や過去配信見たりって感じ！まー若者なら分かるっしょw</p>
                     <br>
-                    <h3>使うのは無料！だけど、いつ規制されてサービス停止されるか分からない！</h3>
-                    <p>ぶっちゃけまじで無法地帯だからいつ規制されてもおかしくない！でもまー立ち位置的には普通のSNSだし使うにあたって身分提示するような事もないし、何かあっても身バレしないのはデカイかな！<br><br>まー不安な人はやめといたほうがいいかもだけど、それでもまじで刺激的過ぎるからそっちの方が要注意かも！普通のAVじゃ満足しなくなるwww</p>
+                    <h3>運が良ければ<br>ガチ素人の生ハメ配信観れるかも..!?</h3>
+                    <p>稀にある生ハメ配信。この配信はなんと<span class="red">同時視聴者数6000人以上</span>を記録。</p>
                     <img src="{{ asset('image/uraLive_5.jpg') }}">
                     <br>
                     <br>
-                    <h3>リンク切れになってたらスマン！！まだリンク生きてたら削除前に急げよ！！！</h3>
-                    <a href="https://www.gamefeat.net/webapi/v1/reportClick?ad_id=8126&site_id=27669" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 公式AppStoreはコチラ <i class="fas fa-external-link-alt"></i></a>
-                    <!--<a href="https://preaf.jp/pa.do?s=v04031&o=46959&guid=ON" target="_blank"><p><span>https://apps.apple.com/jp/app/free/<br>tiktok-ura/goody</p></span></p></a>-->
-                    <img src="{{ asset('image/omi_app.jpg') }}">
-                    <p>↑これな！</p>
-                    <br>
-                    <p>最後にちょっと宣伝！Twitterで裏系・エロ情報発信してこうと思うから、気になるやつはフォローしてやー！@adult_info_</p>
-                    <br>
-                    <div class="line"></div>
+                    <h3>いつアプリ規制されるか分からない<br>サービス停止前に入っていつでも戻れるようにしよう</h3>
+                    <p>これだけの裏サービス。いつ規制されてアプリストアから消されるか分かりません。<br>ただ、<span class="red">一度インストールしてるといつでも戻ってこれる</span>のでまずはインストールだけでもオススメです。</p>
+                    <div class="center">
+                        <a href="https://a-trade.jp/redirect/eazy?media=G16531" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 裏配信はコチラ <i class="fas fa-external-link-alt"></i></a>
+                    </div>
+                    <a href="https://a-trade.jp/redirect/eazy?media=G16531" target="_blank"><img src="https://trading-ad.net/a-trade/banner/eazy_640x200_syogekinoshiyo01_ss50_main.gif" class="wide"></a>
                     <br>
                 </div>
             </div>
-            
-            <!--<div class="ad">-->
-            <!--    <h1><span>【PR】時給1000円以上! アダルトゲームのテスター募集!</span></h1>-->
-                <!--<a href="https://a-trade.jp/redirect/nijikano?media=G16531" target="_blank"><img src="https://trading-ad.net/a-trade/banner/niji_600x500_001.jpg"></a>-->
-            <!--    <a href="https://a-trade.jp/redirect/anahiro?media=G16531" target="_blank"><img src="https://trading-ad.net/a-trade/banner/ana_600x500_009.jpg" alt=""></a>-->
-            <!--    <p><span><i class="far fa-hand-point-up"></i> <i class="far fa-hand-point-up"></i> <i class="far fa-hand-point-up"></i></span></p>-->
-            <!--    <p><span>まず普通にユーザー登録して詳細を確認せよ！</span></p>-->
-            <!--</div>-->
-            <!--<br>-->
         </div>
         <footer>
-            <p>激ヤバ話題まとめ@5ch</p>
+            <p>裏垢女子まとめ@5ch</p>
         </footer>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,9 @@ Route::get('app', function () {
 
 // LP確認用
 Route::get('cccheck', function () {
-    return view('fb.mm'); //獲得LP(4)
+    return view('fb.noMatch'); //獲得LP(2)
     
-    // return view('fb.noMatch'); //獲得LP(2)
+    // return view('fb.mm'); //獲得LP(4)
     // return view('fb.adult'); //獲得LP(3)
     // return view('fb.noMatchRanking'); //獲得LP(3)
     // return view('fb.match'); //獲得LP(1)
